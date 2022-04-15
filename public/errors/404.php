89700199d02fdb8a4a393a1d3392307c317f9693
Antoine Le Bras <alebras@expert-net.fr>
2022-04-15 13:04:12 +0200

    revamp: router

--- public/errors/404.php
+++ public/errors/404.php
@@ -2,5 +2,7 @@
 <p>La page n'existe pas.</p>
 
 <?php 
-echo $_SERVER['DOCUMENT_ROOT'];
+echo $_SERVER['DOCUMENT_ROOT'] . "<br>";
+echo $_SERVER['SERVER_NAME'] . "<br>";
+echo $_SERVER['REQUEST_URI'];
 ?>
--- public/views/home/index.php
+++ public/views/home/index.php
@@ -8,16 +8,8 @@
   <title>Atmos Bakery</title>
 </head>
 <body>
-  <?php
-    require 'src/components/header/header.php';
-  ?>
   <section class="main-section-background">
     C'est la homepage la
-  </section>
-  <section>
-    <?php
-      require 'src/components/footer/footer.php';
-    ?>
   </section>
 </body>
 </html>
