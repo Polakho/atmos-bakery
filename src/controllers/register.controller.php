0337f2d1e79a7c0babc87f9b62d7baa4918e4ec4
Antoine Le Bras <alebras@expert-net.fr>
2022-05-11 15:47:11 +0200

    fix isMailAlreadyUsed

--- src/controllers/register.controller.php
+++ src/controllers/register.controller.php
@@ -9,10 +9,9 @@ $userModel = new UserModel();
 
 if (isset($_POST['name']) && isset($_POST['mail']) && isset($_POST['password']) && isset($_POST['retypePassword'])) {
   $errorMsg = NULL;
-  // if (!$userModel->IsMailAlreadyUsed($_POST['mail'])) {
-  //   $errorMsg = "L'adresse mail est déjà utilisée.";
-  // } else 
-  if ($_POST['password'] != $_POST['retypePassword']) {
+  if (!$userModel->IsMailAlreadyUsed($_POST['mail'])) {
+    $errorMsg = "Cette adresse mail est déjà utilisée.";
+  } else if ($_POST['password'] != $_POST['retypePassword']) {
     $errorMsg = "Les mots de passe ne sont pas les memes.";
   } else if (strlen(trim($_POST['password'])) < 8) {
     $errorMsg = "Votre mot de passe doit contenir 8 caractères au minimum.";
