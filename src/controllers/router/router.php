47b74d919f531a5297d47a0cd410ba49baab98ac
Antoine Le Bras <alebras@expert-net.fr>
2022-05-11 13:51:33 +0200

    before classes merge

--- src/controllers/router/router.php
+++ src/controllers/router/router.php
@@ -6,6 +6,9 @@ switch ($action) {
   case 'about':
     require "../src/controllers/about.controller.php";
     break;
+  case 'register':
+    require "../src/controllers/register.controller.php";
+    break;
   default:
     require "../src/controllers/home.controller.php";
     break;
