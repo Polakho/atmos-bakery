a3c782d3b9574315dc6d66a89f8a4aa61006688c
Antoine Le Bras <alebras@expert-net.fr>
2022-07-09 12:11:29 +0200

    fix(footer): right css

--- src/components/footer/footer.php
+++ src/components/footer/footer.php
@@ -2,14 +2,14 @@
   <footer>
     <ul class="content-link">
       <li><a href="/about">À propos</a></li>
-      <li><a href="">Contacter</a></li>
+      <!-- <li><a href="">Contacter</a></li> -->
       <li><a href="https://gitlab.com/a8979/atmos-bakery-front" target="_blank">Code source</a></li>
     </ul>
 
     <ul class="socials">
-      <li><img src="../../assets/img/socials/facebook-brands.svg" alt="facebook"></li>
-      <li><img src="../../assets/img/socials/twitter-brands.svg" alt="twitter"></li>
-      <li><img src="../../assets/img/socials/instagram-brands.svg" alt="instagram"></li>
+      <li><a href=""><img src="../../assets/img/socials/facebook-brands.svg" alt="facebook"></a></li>
+      <li><a href=""><img src="../../assets/img/socials/twitter-brands.svg" alt="twitter"></a></li>
+      <li><a href=""><img src="../../assets/img/socials/instagram-brands.svg" alt="instagram"></a></li>
     </ul>
   </footer>
 </html>
