aa0dc20cfd10ca8d582b10f23cee17a40687f684
Polakho <armatys.alex@gmail.com>
2022-07-18 14:58:39 +0200

    Essaie de Css Panier

--- src/components/modales/cartModale.php
+++ src/components/modales/cartModale.php
@@ -1,6 +1,6 @@
 <div class=" modal modal-cart hidden">
     <div class="modal-header">
-        <h1>Mon panier</h1>
+        <h3>Mon panier</h3>
         <span class="close">&times;</span>
     </div>
     <div class="modal-body">
